3fa6281e932f8226f3e62596c04e15ac0b86ac49
Ajay702 <74392659+Ajay702@users.noreply.github.com>
2024-06-30 14:15:43 +0530

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -23,7 +23,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/data');
+        const response = await fetch('https://dashboard-one-delta-61.vercel.app/');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
